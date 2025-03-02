ebe1db3c0c0c15db183fd885de0513cd57f4787f
jd-galvan <jdgsuazo@gmail.com>
2025-03-02 16:04:07 +0100

    complete route of checkpoint

--- libs/sam2/model.py
+++ libs/sam2/model.py
@@ -24,7 +24,7 @@ class SAM2:
         else:
             print("El modelo ya está descargado.")
 
-        sam_checkpoint = "sam_vit_h_4b8939.pth"
+        sam_checkpoint = "./checkpoints/sam_vit_h_4b8939.pth"
         model_type = "vit_h"
 
         sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
